314ed465cc414e3c685eae7f892980c1d3517463
Shaun Liew Xin Hong <shaunliew20@gmail.com>
2022-10-24 23:05:51 +0800

    add map api and map into the main screen

--- lib/tabPages/home_tab.dart
+++ lib/tabPages/home_tab.dart
@@ -1,4 +1,7 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class HomeTab extends StatefulWidget {
   const HomeTab({super.key});
@@ -8,10 +11,27 @@ class HomeTab extends StatefulWidget {
 }
 
 class _HomeTabState extends State<HomeTab> {
+  late GoogleMapController newGoogleMapController;
+  final Completer<GoogleMapController> _controllerGoogleMap = Completer();
+  static const CameraPosition _kGooglePlex = CameraPosition(
+    target: LatLng(37.42796133580664, -122.085749655962),
+    zoom: 14.4746,
+  );
+
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text("Home"),
+    return Stack(
+      children: [
+        GoogleMap(
+          mapType: MapType.normal,
+          myLocationEnabled: true,
+          initialCameraPosition: _kGooglePlex,
+          onMapCreated: (GoogleMapController controller) {
+            _controllerGoogleMap.complete(controller);
+            newGoogleMapController = controller;
+          },
+        ),
+      ],
     );
   }
 }
