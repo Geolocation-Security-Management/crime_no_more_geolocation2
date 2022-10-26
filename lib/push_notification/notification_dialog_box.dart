cbf50257a2e2f09190657cee19b0af8708d78496
Shaun Liew Xin Hong <shaunliew20@gmail.com>
2022-10-26 23:18:48 +0800

    implement push notif and create notif dialog box

---
+++ lib/push_notification/notification_dialog_box.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+import 'package:flutter/material.dart';
+
+class NotificationDialogBox extends StatefulWidget {
+  const NotificationDialogBox({super.key});
+
+  @override
+  State<NotificationDialogBox> createState() => _NotificationDialogBoxState();
+}
+
+class _NotificationDialogBoxState extends State<NotificationDialogBox> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
--- lib/push_notification/push_notification_system.dart
+++ lib/push_notification/push_notification_system.dart
@@ -1,6 +1,9 @@
 import 'package:crime_no_more_geolocation2/global/global.dart';
+import 'package:crime_no_more_geolocation2/models/event_request_information.dart';
 import 'package:firebase_database/firebase_database.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class PushNotificationSystem {
   FirebaseMessaging messaging = FirebaseMessaging.instance;
@@ -13,19 +16,59 @@ class PushNotificationSystem {
         .then((RemoteMessage? remoteMessage) {
       if (remoteMessage != null) {
         //display the guards request information - the location and the crime details
-
+        readControlRoomEventRequestInformation(
+            remoteMessage.data["eventRequestId"]);
       }
     });
     //2. Foreground state
     //When the app is open and it receives a push notification
     FirebaseMessaging.onMessage.listen((RemoteMessage? remoteMessage) {
       //display the guards request information - the location and the crime details
+      readControlRoomEventRequestInformation(
+          remoteMessage?.data["eventRequestId"]);
     });
 
     //3. Background state
     //When the app is in the background and opened directly from the push notification
     FirebaseMessaging.onMessageOpenedApp.listen((RemoteMessage? remoteMessage) {
       //display the guards request information - the location and the crime details
+      readControlRoomEventRequestInformation(
+          remoteMessage?.data["eventRequestId"]);
+    });
+  }
+
+  readControlRoomEventRequestInformation(String eventRequestId) {
+    //specific eventRequestId in the realtime database
+    FirebaseDatabase.instance
+        .ref()
+        .child("event")
+        .child(eventRequestId)
+        .once()
+        .then((snapData) {
+      if (snapData.snapshot.value != null) {
+        // coordinates of the crime
+        double destinationLat = double.parse(
+            (snapData.snapshot.value! as Map)["coordinate"]["lat"].toString());
+        double destinationLng = double.parse(
+            (snapData.snapshot.value! as Map)["coordinate"]["lng"].toString());
+        String details =
+            (snapData.snapshot.value! as Map)["details"].toString();
+        String region = (snapData.snapshot.value! as Map)["region"].toString();
+        String severity_level =
+            (snapData.snapshot.value! as Map)["severity_level"].toString();
+        String status = (snapData.snapshot.value! as Map)["status"].toString();
+
+        EventRequestInformation eventRequestDetails = EventRequestInformation();
+
+        eventRequestDetails.destinationLatLng =
+            LatLng(destinationLat, destinationLng); //coordinates of the crime
+        eventRequestDetails.details = details;
+        eventRequestDetails.region = region;
+        eventRequestDetails.severity_level = severity_level;
+        eventRequestDetails.status = status;
+      } else {
+        Fluttertoast.showToast(msg: "This event Request ID does not exist");
+      }
     });
   }
 
